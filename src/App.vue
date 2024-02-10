b38bf5c68681f406c712f0a5b6987e0da00e4863
Mavv3006 <marvdeus@gmail.com>
2024-02-10 13:24:18 +0100

    finished the challenge

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,78 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+  <div class="outer-wrapper">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="avatar">
+        <img src="./assets/images/avatar-jessica.jpeg" />
+      </div>
+      <div class="name">Jessica Randall</div>
+      <div class="location">London, United Kingdom</div>
+      <div class="quote">"Front-end developer and avid reader."</div>
+      <div class="buttons">
+        <a>GitHub</a>
+        <a>Frontend Mentor</a>
+        <a>LinkedIn</a>
+        <a>Twitter</a>
+        <a>Instagram</a>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup lang="ts"></script>
+
+<style>
+.outer-wrapper {
+  padding-top: 40px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper {
+  margin: auto;
+  width: 375px;
+  border-radius: 10px;
+  text-align: center;
+  background-color: hsl(0, 0%, 12%);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.avatar img {
+  margin-top: 35px;
+  border-radius: 50%;
+  width: 90px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.name {
+  font-weight: 600;
+  margin-top: 1.125em;
+  font-size: 1.5rem;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.location {
+  margin-top: 0.75em;
+  margin-bottom: 2em;
+  color: hsl(75, 94%, 57%);
+  font-weight: 600;
+}
+
+.buttons {
+  margin-top: 1.75em;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+
+.buttons a {
+  font-weight: 600;
+  width: full;
+  text-align: center;
+  padding: 13px 0;
+  border-radius: 7px;
+  background-color: hsl(0, 0%, 20%);
+}
+.buttons a:hover {
+  background-color: hsl(75, 94%, 57%);
+  cursor: pointer;
+  color: hsl(0, 0%, 12%);
 }
 </style>
